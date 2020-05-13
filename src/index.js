36d8d1bddc3207ab37ed381815499cd26d1898a5
脱安琪 <453001363@qq.com>
2020-05-13 15:40:57 +0800

    feat: 引入jQuery

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import _ from 'lodash';
+import j from 'jquery'
 
+
+  // 引入jQuery
   function component() {
-    let element = document.createElement('div');
+    
+    // let element = document.createElement('div');
+    
+    // jQuery
+    let element = j('<div></div>');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    
+    // jQuery
+    element.html(_.join(['Hello','webpack'], ' '))
 
-    return element;
+    // return element;
+    return element.get(0);
   }
+
+
+  // 没有引入jQuery
+  // function componentNojQuery() {
+  //   let element = document.createElement('div');
+  //   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  //   return element;
+  // }
 
 document.body.appendChild(component());
